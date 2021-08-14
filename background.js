f9bfb80e85cb808f0cccfed073b35787e03d94d3
Jaime <jaimeloeuf@gmail.com>
2021-08-14 17:22:13 +0800

    Context menu to open incognito tab in new window:     Add a new context menu that will open the current tab in a new normal     window if the current tab is a incognito tab.

--- background.js
+++ background.js
@@ -1,3 +1,30 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("Uncognito installed");
+chrome.runtime.onInstalled.addListener((installationObject) => {
+  if (installationObject.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+    console.log("Uncognito installed");
+  }
+});
+
+chrome.contextMenus.create({
+  title: "Uncognito this tab",
+  contexts: [chrome.contextMenus.ContextType.ALL],
+  id: "context",
+});
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  if (tab.incognito) {
+    const tabUrl = tab.url || tab.pendingUrl;
+
+    chrome.windows.create({
+      // opens a normal active window
+      focused: true,
+      type: "normal",
+
+      // Open in a normal window
+      incognito: false,
+
+      // tabId: tab.id,
+
+      url: [tabUrl],
+    });
+  }
 });
