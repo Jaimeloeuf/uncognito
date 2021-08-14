b6cb7e6422f4a0f96a11f63075c73f53eecb03f0
Jaime <jaimeloeuf@gmail.com>
2021-08-15 00:50:10 +0800

    Refactor popup UI logic to mount programmatically:     Build the popup UI entirely in JS, with 2 different functions for     2 popup UI scenarios, 1 for error and 1 for normal operation, and     mount a popup UI programmatically based on whether the extension is     allowed to run in incognito mode.

--- popup.js
+++ popup.js
@@ -1,70 +1,74 @@
-// Check if extension have access to incognito windows/tabs
-chrome.extension.isAllowedIncognitoAccess((isAllowed) => {
-  if (isAllowed) {
-    // document.body.appendChild();
-  } else {
-    const noAccessError = document.createElement("h2");
-    noAccessError.innerHTML =
-      "Please enable incognito access to use extension in extension's settings page";
-    noAccessError.style = "color: rgb(255, 90, 90)";
+function mountPopup() {
+  /* Button for re-opening entire incognito window */
+  const reopenWindowBtn = document.createElement("button");
+  reopenWindowBtn.innerHTML = "Re-Open <b>current window</b> in normal window";
+  reopenWindowBtn.style = "background-color: rgb(220, 220, 220)";
+  reopenWindowBtn.onclick = async function () {
+    // Get current window to check if it is a incognito window
+    const { id, incognito } = await chrome.windows.getCurrent();
 
-    const buttonToExtensionsPage = document.createElement("button");
-    buttonToExtensionsPage.innerHTML = "Extension's settings";
-    buttonToExtensionsPage.style = "background-color: rgb(253, 222, 227);";
-    buttonToExtensionsPage.onclick = function () {
-      chrome.tabs.create({
-        // Create tab and focus on it
-        active: true,
+    // Only reopen the tabs in new window if the current window is a incognito window
+    if (incognito) {
+      // Instead of currentWindow, windowId can be used too, but this just simplifies the readability of the API usage
+      const tabs = await chrome.tabs.query({ currentWindow: true });
 
-        // From chrome extension root URL to chrome extension settings page
-        // chrome-extension://aeghlmlmidbengggkhfocdkdhejpmfjh/
-        // chrome://extensions/?id=aeghlmlmidbengggkhfocdkdhejpmfjh
-        url:
-          "chrome://extensions/?id=" +
-          chrome.runtime
-            .getURL("")
-            .replace("chrome-extension://", "")
-            .replace("/", ""),
+      // Extract out only the URLs of the tab(s) array
+      const tabURLs = tabs.map((tab) => tab.url);
+
+      // Await for new window to be created first before closing the incognito window
+      await chrome.windows.create({
+        // Opens a normal active window
+        focused: true,
+        type: "normal",
+
+        // Open in a normal window
+        incognito: false,
+
+        // Have to use URL instead of tab IDs because tabs can only be moved between windows of the same profile
+        // i.e incognito tabs can only be moved to another incognito window
+        url: tabURLs,
       });
-    };
 
-    document.body.appendChild(noAccessError);
-    document.body.appendChild(buttonToExtensionsPage);
-  }
+      // Close the entire incognito window and all tabs in it once the new window is created
+      await chrome.windows.remove(id);
+    }
+  };
+
+  document.body.appendChild(reopenWindowBtn);
+}
+
+function mountErrorPopup() {
+  const noAccessError = document.createElement("h2");
+  noAccessError.innerHTML =
+    "Please enable incognito access to use extension in extension's settings page";
+  noAccessError.style = "color: rgb(255, 90, 90)";
+
+  const buttonToExtensionsPage = document.createElement("button");
+  buttonToExtensionsPage.innerHTML = "Extension's settings";
+  buttonToExtensionsPage.style = "background-color: rgb(253, 222, 227);";
+  buttonToExtensionsPage.onclick = function () {
+    chrome.tabs.create({
+      // Create tab and focus on it
+      active: true,
+
+      // From chrome extension root URL to chrome extension settings page
+      // chrome-extension://aeghlmlmidbengggkhfocdkdhejpmfjh/
+      // chrome://extensions/?id=aeghlmlmidbengggkhfocdkdhejpmfjh
+      url:
+        "chrome://extensions/?id=" +
+        chrome.runtime
+          .getURL("")
+          .replace("chrome-extension://", "")
+          .replace("/", ""),
+    });
+  };
+
+  document.body.appendChild(noAccessError);
+  document.body.appendChild(buttonToExtensionsPage);
+}
+
+// Check if extension have access to incognito windows/tabs and mount a specific popup UI
+chrome.extension.isAllowedIncognitoAccess((isAllowed) => {
+  if (isAllowed) mountPopup();
+  else mountErrorPopup();
 });
-
-const reopenWindowBtn = document.getElementById("reopen-window");
-
-// When the button is clicked, inject setPageBackgroundColor into current page
-reopenWindowBtn.onclick = async function () {
-  // Get current window to check if it is a incognito window
-  const { id, incognito } = await chrome.windows.getCurrent();
-
-  // Only reopen the tabs in new window if the current window is a incognito window
-  if (incognito) {
-    const tabs = await chrome.tabs.query({
-      // Instead of currentWindow, windowId can be used too, but this just simplifies the API use
-      currentWindow: true,
-    });
-
-    // Extract out only the URLs of the tab(s) array
-    const tabURLs = tabs.map((tab) => tab.url);
-
-    // Await for new window to be created first before closing the incognito window
-    await chrome.windows.create({
-      // Opens a normal active window
-      focused: true,
-      type: "normal",
-
-      // Open in a normal window
-      incognito: false,
-
-      // Have to use URL instead of tab IDs because tabs can only be moved between windows of the same profile
-      // i.e incognito tabs can only be moved to another incognito window
-      url: tabURLs,
-    });
-
-    // Close the entire incognito window and all tabs in it once the new window is created
-    await chrome.windows.remove(id);
-  }
-};
