13f852c52f72c614b11f443755dd93f005ebc475
Jaime <jaimeloeuf@gmail.com>
2024-06-15 14:45:48 +0800

    Refactor out errorPopup UI.

---
+++ src/errorPopup.js
@@ -0,0 +1,34 @@
+// Error popup UI
+export function errorPopup() {
+  const noAccessError = document.createElement("h2");
+  noAccessError.innerHTML =
+    "Please enable incognito access to use extension in extension's settings page";
+  noAccessError.style = "color: rgb(255, 90, 90)";
+
+  const buttonToExtensionsPage = document.createElement("button");
+  buttonToExtensionsPage.innerHTML = "Extension's settings";
+  buttonToExtensionsPage.style = "background-color: rgb(253, 222, 227);";
+  buttonToExtensionsPage.onclick = function () {
+    chrome.tabs.create({
+      // Create tab and focus on it
+      active: true,
+
+      // From chrome extension root URL to chrome extension settings page
+      // chrome-extension://aeghlmlmidbengggkhfocdkdhejpmfjh/
+      // chrome://extensions/?id=aeghlmlmidbengggkhfocdkdhejpmfjh
+      url:
+        "chrome://extensions/?id=" +
+        chrome.runtime
+          .getURL("")
+          .replace("chrome-extension://", "")
+          .replace("/", ""),
+    });
+  };
+
+  /* Div to group everything together to return as a single element */
+  const div = document.createElement("div");
+  div.appendChild(noAccessError);
+  div.appendChild(buttonToExtensionsPage);
+
+  return div;
+}
--- src/popup.js
+++ src/popup.js
@@ -1,4 +1,5 @@
 import { createFlexRowDiv } from "./createFlexRowDiv.js";
+import { errorPopup } from "./errorPopup.js";
 import { isJSON } from "./isJSON.js";
 
 // Function to create and return HTML Div element of the `link reopening section`
@@ -229,41 +230,6 @@ function linksManipulationDiv() {
   div.appendChild(
     createFlexRowDiv(reopenLinksInSameWindowBtn, reopenLinksInNewWindowBtn)
   );
-
-  return div;
-}
-
-// Error popup UI
-function errorPopup() {
-  const noAccessError = document.createElement("h2");
-  noAccessError.innerHTML =
-    "Please enable incognito access to use extension in extension's settings page";
-  noAccessError.style = "color: rgb(255, 90, 90)";
-
-  const buttonToExtensionsPage = document.createElement("button");
-  buttonToExtensionsPage.innerHTML = "Extension's settings";
-  buttonToExtensionsPage.style = "background-color: rgb(253, 222, 227);";
-  buttonToExtensionsPage.onclick = function () {
-    chrome.tabs.create({
-      // Create tab and focus on it
-      active: true,
-
-      // From chrome extension root URL to chrome extension settings page
-      // chrome-extension://aeghlmlmidbengggkhfocdkdhejpmfjh/
-      // chrome://extensions/?id=aeghlmlmidbengggkhfocdkdhejpmfjh
-      url:
-        "chrome://extensions/?id=" +
-        chrome.runtime
-          .getURL("")
-          .replace("chrome-extension://", "")
-          .replace("/", ""),
-    });
-  };
-
-  /* Div to group everything together to return as a single element */
-  const div = document.createElement("div");
-  div.appendChild(noAccessError);
-  div.appendChild(buttonToExtensionsPage);
 
   return div;
 }
